8f2620099a2fa22515429a22d9c80d6243678d1e
salmindev <salmin20shafii@gmail.com>
2023-06-23 14:04:06 +0300

    changes

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-import { Card, Button } from 'react-bootstrap';
+
 
 const Home= ({item, handleClick}) => {
   const [products, setProducts] = useState([]);
